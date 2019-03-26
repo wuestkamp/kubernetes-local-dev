f0ecfdbc7fdd2e854cb01feaf34444577ffd28c0
Kim Wuestkamp <kim@wuestkamp.com>
2019-03-26 22:59:45 +0100

    container naming

--- run/build.sh
+++ run/build.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
 CONTAINER_REPO="localhost:5000"
+CONTAINER_PHP=$CONTAINER_REPO/wuestkamp_php
+CONTAINER_NGINX=$CONTAINER_REPO/wuestkamp_nginx
 
 yarn --cwd symfony install
 yarn --cwd symfony encore dev
 
-docker build -t $CONTAINER_REPO/wuestkamp_php:latest -f infrastructure/php-fpm/Dockerfile --build-arg build_path=infrastructure/php-fpm . #--no-cache
-docker build -t $CONTAINER_REPO/wuestkamp_nginx:latest -f infrastructure/nginx/Dockerfile --build-arg build_path=infrastructure/nginx . #--no-cache
+docker build -t $CONTAINER_PHP:latest -f infrastructure/php-fpm/Dockerfile --build-arg build_path=infrastructure/php-fpm . #--no-cache
+docker build -t $CONTAINER_NGINX:latest -f infrastructure/nginx/Dockerfile --build-arg build_path=infrastructure/nginx . #--no-cache
 
-docker push $CONTAINER_REPO/wuestkamp_php:latest
-docker push $CONTAINER_REPO/wuestkamp_nginx:latest
+docker push $CONTAINER_PHP:latest
+docker push $CONTAINER_NGINX:latest
--- run/info.sh
+++ run/info.sh
@@ -1,1 +1,3 @@
+#!/usr/bin/env bash
+
 watch -n 0.5 "kubectl config current-context; echo ''; kubectl config view | grep namespace; echo ''; kubectl get namespace,node,ingress,pod,svc,job,cronjob,deployment,rs,pv,pvc,secret,ep -o wide"
