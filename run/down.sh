57e9ca7006575cdf889d6552691847f5d708ac9c
Kim Wuestkamp <kim@wuestkamp.com>
2019-03-26 22:08:49 +0100

    nginx and php in one pod

--- run/down.sh
+++ run/down.sh
@@ -2,10 +2,13 @@
 
 K8N_PATH="infrastructure/kubernetes"
 
-kubectl delete -f $K8N_PATH/nginx-deployment.yaml
-kubectl delete -f $K8N_PATH/nginx-service.yaml
-kubectl delete -f $K8N_PATH/php-deployment.yaml
-kubectl delete -f $K8N_PATH/php-service.yaml
-kubectl delete -f $K8N_PATH/nginx-claim0-persistentvolumeclaim.yaml
-kubectl delete -f $K8N_PATH/php-claim0-persistentvolumeclaim.yaml
-kubectl delete -f $K8N_PATH/php-claim1-persistentvolumeclaim.yaml
+#kubectl delete -f $K8N_PATH/nginx-deployment.yaml
+#kubectl delete -f $K8N_PATH/nginx-service.yaml
+#kubectl delete -f $K8N_PATH/php-deployment.yaml
+#kubectl delete -f $K8N_PATH/php-service.yaml
+#kubectl delete -f $K8N_PATH/nginx-claim0-persistentvolumeclaim.yaml
+#kubectl delete -f $K8N_PATH/php-claim0-persistentvolumeclaim.yaml
+#kubectl delete -f $K8N_PATH/php-claim1-persistentvolumeclaim.yaml
+
+kubectl delete -f $K8N_PATH/sf-deployment.yaml
+kubectl delete -f $K8N_PATH/sf-service.yaml
--- run/up.sh
+++ run/up.sh
@@ -2,16 +2,20 @@
 
 K8N_PATH="infrastructure/kubernetes"
 
-kubectl create -f $K8N_PATH/php-claim0-persistentvolumeclaim.yaml
-kubectl create -f $K8N_PATH/php-claim1-persistentvolumeclaim.yaml
-sleep 1
-kubectl create -f $K8N_PATH/php-deployment.yaml
-sleep 1
-kubectl create -f $K8N_PATH/php-service.yaml
-sleep 1
+#kubectl create -f $K8N_PATH/php-claim0-persistentvolumeclaim.yaml
+#kubectl create -f $K8N_PATH/php-claim1-persistentvolumeclaim.yaml
+#sleep 1
+#kubectl create -f $K8N_PATH/php-deployment.yaml
+#sleep 1
+#kubectl create -f $K8N_PATH/php-service.yaml
+#sleep 1
 
-kubectl create -f $K8N_PATH/nginx-claim0-persistentvolumeclaim.yaml
-sleep 1
-kubectl create -f $K8N_PATH/nginx-deployment.yaml
-sleep 1
-kubectl create -f $K8N_PATH/nginx-service.yaml
+#kubectl create -f $K8N_PATH/nginx-claim0-persistentvolumeclaim.yaml
+#sleep 1
+#kubectl create -f $K8N_PATH/nginx-deployment.yaml
+#sleep 1
+#kubectl create -f $K8N_PATH/nginx-service.yaml
+
+
+kubectl create -f $K8N_PATH/sf-deployment.yaml
+kubectl create -f $K8N_PATH/sf-service.yaml
