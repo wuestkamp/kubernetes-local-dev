65cb50b668a9579d215a59932ab81fbc94466fee
Kim Wuestkamp <kim@wuestkamp.com>
2019-04-11 17:20:44 +0200

    info without all namespaces

--- run/info.sh
+++ run/info.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-watch "kubectl config current-context; echo ''; kubectl config view | grep namespace; echo ''; kubectl get namespace,node,ingress,pod,svc,job,cronjob,deployment,rs,pv,pvc,secret,configmap,ep -o wide --all-namespaces"
+watch "kubectl config current-context; echo ''; kubectl config view | grep namespace; echo ''; kubectl get namespace,node,ingress,pod,svc,job,cronjob,deployment,rs,pv,pvc,secret,configmap,ep -o wide"
