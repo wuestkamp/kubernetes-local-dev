f354b4531b16e0e8a8c7f491ff871da5e9340914
Kim Wuestkamp <kim@wuestkamp.com>
2019-04-17 19:40:50 +0200

    updated up and down scripts

--- run/down.sh
+++ run/down.sh
@@ -1,6 +1,2 @@
 #!/usr/bin/env bash
-
-K8N_PATH="infrastructure/kubernetes"
-
-kubectl delete -f $K8N_PATH/sf-deployment.yaml
-kubectl delete -f $K8N_PATH/sf-service.yaml
+helm del --purge sf
--- run/up.sh
+++ run/up.sh
@@ -1,6 +1,2 @@
 #!/usr/bin/env bash
-
-K8N_PATH="infrastructure/kubernetes"
-
-kubectl create -f $K8N_PATH/sf-deployment.yaml
-kubectl create -f $K8N_PATH/sf-service.yaml
+helm install --name sf infrastructure/helm/sf --set environment=${1:-dev}
