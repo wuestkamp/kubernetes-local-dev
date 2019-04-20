ae361770ebc12e00f439080f8ef718e421a09e84
Kim Wuestkamp <kim@wuestkamp.com>
2019-04-20 22:47:48 +0200

    added postgresql

--- run/up.sh
+++ run/up.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-helm install --name sf infrastructure/helm/sf --set environment=${1:-dev} --set php.symfony.appEnv=${1:-dev}
+helm install --name sf infrastructure/helm/sf --set environment=${1:-dev} --set php.symfony.appEnv=${1:-dev} #--set postgresqlUsername=pg --set postgresqlPassword=abcdef --set postgresqlDatabase=symfony
---
+++ symfony/src/Entity/Product.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ */
+class Product
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+}
