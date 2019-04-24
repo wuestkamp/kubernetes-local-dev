17a9ee85abdb65914efce43adbd5bcb1c975819a
Kim Wuestkamp <kim@wuestkamp.com>
2019-04-24 22:53:55 +0200

    enable xdebug only in dev environment

--- run/up.sh
+++ run/up.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 kubectl create secret tls nginxsecret --key infrastructure/certs/nginx/server.key --cert infrastructure/certs/nginx/server.csr
+kubectl create configmap php-xdebug-config --from-file infrastructure/php-fpm/xdebug.ini
 helm install --name sf infrastructure/helm/sf --set environment=${1:-dev} --set php.symfony.appEnv=${1:-dev}
