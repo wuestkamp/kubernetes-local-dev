11e2ee0dc8187dddf5b491ea42cff83715ce2b20
Kim Wuestkamp <kim@wuestkamp.com>
2019-04-24 23:38:53 +0200

    helm value for xdebug

--- run/up.sh
+++ run/up.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 kubectl create secret tls nginxsecret --key infrastructure/certs/nginx/server.key --cert infrastructure/certs/nginx/server.csr
 kubectl create configmap php-xdebug-config --from-file infrastructure/php-fpm/xdebug.ini
-helm install --name sf infrastructure/helm/sf --set environment=${1:-dev} --set php.symfony.appEnv=${1:-dev}
+
+if [ "$1" == "prod" ]; then
+    helm install --name sf infrastructure/helm/sf --set environment=${1:-dev} --set php.symfony.appEnv=${1:-dev} --set php.xdebug=false
+else
+    helm install --name sf infrastructure/helm/sf --set environment=${1:-dev} --set php.symfony.appEnv=${1:-dev}
+fi
